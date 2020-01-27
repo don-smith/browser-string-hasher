57c34b0f89ca9a94a8aef7aadf8b80ae93a60e68
Don Smith <don@antiantonym.com>
2020-01-27 16:00:19 +1300

    rename and apply preferred code style

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-# Easy Hash
+# Browser String Hasher
+
 A barebones function for getting a SHA256 hash of a string using the browser's native crypto module.
 
 ### Usage
-```javascript
-import sha from "simple-hash-browser";
 
-sha("String To Hash Beyond Recognition");
- // returns a Promise
+```js
+import sha from "browser-string-hasher"
 
-sha("This other string").then(hash => {console.log(hash)});
+sha("This other string")
+  .then(hash => {
+    console.log(hash)
+  })
+
 // "0d5a0597790745345842b61356016e3b24e83a7e6b0266618180c3a864bf25f1"
 ```
--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-function sha(str) {
-  var buffer = new TextEncoder("utf-8").encode(str);
-  return crypto.subtle.digest("SHA-256", buffer).then(function (hash) {
-    return hex(hash);
-  });
+export default function sha (str) {
+  const buffer = new TextEncoder('utf-8').encode(str)
+  return crypto.subtle.digest('SHA-256', buffer).then(makeHexCodes)
 }
 
-function hex(buffer) {
-  var hexCodes = [];
-  var view = new DataView(buffer);
-  for (var i = 0; i < view.byteLength; i += 4) {
+function makeHexCodes (buffer) {
+  const hexCodes = []
+  const view = new DataView(buffer)
+  for (let i = 0; i < view.byteLength; i += 4) {
     // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
-    var value = view.getUint32(i)
+    const value = view.getUint32(i)
+
     // toString(16) will give the hex representation of the number without padding
-    var stringValue = value.toString(16)
+    const stringValue = value.toString(16)
+
     // We use concatenation and slice for padding
-    var padding = '00000000'
-    var paddedValue = (padding + stringValue).slice(-padding.length)
-    hexCodes.push(paddedValue);
+    const padding = '00000000'
+    const paddedValue = (padding + stringValue).slice(-padding.length)
+
+    hexCodes.push(paddedValue)
   }
 
   // Join all the hex strings into one
-  return hexCodes.join("");
+  return hexCodes.join('')
 }
-
-module.exports = sha;
